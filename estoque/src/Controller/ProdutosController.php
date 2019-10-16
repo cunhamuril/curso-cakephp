5bc140a94ae917903d4aa3264af4455e387970d1
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-10-16 17:13:04 -0300

    listando produtos

--- estoque/src/Controller/ProdutosController.php
+++ estoque/src/Controller/ProdutosController.php
@@ -3,14 +3,36 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
 
 class ProdutosController extends AppController
 {
   public function index()
   {
-    $msg = "Bem vindos ao cake php!
-      (esta msg veio do controller)";
+    // $msg = "Bem vindos ao cake php!
+    //   (esta msg veio do controller)";
 
-    $this->set('msg', $msg);
+    // $this->set('msg', $msg);    
+
+    $produtosTable = TableRegistry::get('Produtos');
+    $produtos = $produtosTable->find('all');
+
+    // $produtos = [];
+    // $produtos[] = [
+    //   "id" => 1,
+    //   "nome" => "HD de 20 gigas",
+    //   "preco" => 29.99,
+    //   "descricao" => "hd muito bom da marca mega hd"
+    // ];
+
+    // $produtos[] = [
+    //   "id" => 2,
+    //   "nome" => "Playstation",
+    //   "preco" => 999.99,
+    //   "descricao" => "Videogame mto bom pra perder o tempo"
+    // ];
+
+    // $this->set('produto', $produto);
+    $this->set('produtos', $produtos);
   }
 }
--- estoque/src/Template/Produtos/index.ctp
+++ estoque/src/Template/Produtos/index.ctp
@@ -1,1 +1,24 @@
-<h1><?= $msg; ?></h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Nome</th>
+      <th>Preço</th>
+      <th>Descrição</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    foreach ($produtos as $produto) {;
+      ?>
+      <tr>
+        <td><?= $produto['id'] ?></td>
+        <td><?= $produto['nome'] ?></td>
+        <td><?= $produto['preco'] ?></td>
+        <td><?= $produto['descricao'] ?></td>
+      </tr>
+    <?php
+    }
+    ?>
+  </tbody>
+</table>
